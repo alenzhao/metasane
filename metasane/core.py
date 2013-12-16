46fb7288d3ecbc6bc83568560a6f5f8c05ac9b3b
Jai Ram Rideout <jai.rideout@gmail.com>
2013-12-16 14:16:16 -0500

    Add VocabularySet class
    MetadataTable can now identify columns that are linked to an ontology with (optionally) more specificity.

--- metasane/core.py
+++ metasane/core.py
@@ -3,6 +3,8 @@ from __future__ import division
 
 from csv import DictReader
 from collections import defaultdict
+from glob import glob
+from os.path import basename, join, splitext
 
 class MetadataTable(object):
     OntologyDelimiter = ':'
@@ -12,7 +14,7 @@ class MetadataTable(object):
         self.FieldNames = reader.fieldnames
         self._table = [row for row in reader]
 
-    def candidateOntologyColumns(self):
+    def candidateOntologyColumns(self, vocab=None):
         ont_cols = defaultdict(set)
 
         for row in self._table:
@@ -20,6 +22,34 @@ class MetadataTable(object):
                 split_val = value.split(self.OntologyDelimiter, 1)
 
                 if len(split_val) > 1:
-                    ont_cols[field].add(split_val[0])
+                    ont_id = split_val[0]
+
+                    if vocab is None or ont_id in vocab:
+                        ont_cols[field].add(ont_id)
 
         return ont_cols
+
+class VocabularySet(object):
+    Wildcard = '*.txt'
+
+    def __init__(self, vocab_dir):
+        self._vocabs = {}
+
+        for fp in glob(join(vocab_dir, self.Wildcard)):
+            vocab_id = splitext(basename(fp))[0]
+            vocab = set()
+
+            with open(fp, 'U') as f:
+                for line in f:
+                    line = line.strip()
+
+                    if line:
+                        vocab.add(line)
+
+            self._vocabs[vocab_id] = vocab
+
+    def __contains__(self, key):
+        return key in self._vocabs
+
+    def __getitem__(self, key):
+        return self._vocabs[key]
